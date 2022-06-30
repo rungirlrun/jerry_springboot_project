3be5f14b66b0b8150913ffc583ae7473c8a76f1d
제리 <juwonlee920@gmail.com>
2022-06-30 15:23:45 +0900

    스프링 시큐리티 적용

---
+++ sbb/src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().antMatchers("/**").permitAll()
+		.and()
+			.csrf().ignoringAntMatchers("/h2-console/**")					// h2-console 접속시 403 Forbidden이 뜨는 것을 방지
+		.and()
+			.headers()
+			.addHeaderWriter(new XFrameOptionsHeaderWriter(
+					XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))	// h2-console 접속시 화면 깨짐을 방지. 화면깨짐의 원인 :  H2 콘솔의 화면이 frame 구조로 작성,스프링 시큐리티는 사이트의 콘텐츠가 다른 사이트에 포함되지 않도록 하기 위해 X-Frame-Options 헤더값을 사용하여 이를 방지 
+			;
+	}
+}
